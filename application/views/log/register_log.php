10bce62b5290bd0015ced1dcad14cbae85ab9e44
riyansasaja <riyanboyuhu@gmail.com>
2022-10-12 23:15:25 +0800

    menambah log registerlog.php

---
+++ application/views/log/register_log.php
@@ -0,0 +1,29 @@
+<main class="mt-5">
+
+    <div class="container-fluid px-4 ">
+        <div class="row">
+            <div class="col">
+
+                <div class="table-responsive">
+                    <table class="table" id="listperkara">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Log</th>
+                                <th>Waktu Rekam</th>
+                                <th>User</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+
+</main>
