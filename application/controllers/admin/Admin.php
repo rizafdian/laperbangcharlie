e18a73c9bddbabc4dcde10c36170cc44e63d2bb0
rizafdian <afdianriz@gmail.com>
2023-08-16 14:22:12 +0700

    coba update status10

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -366,37 +366,37 @@ class Admin extends CI_Controller
 
         $this->db->set('rekam_log', 'NOW()', FALSE);
         $this->db->insert('log_audittrail', $audittrail);
+            //API Notifikasi WA
+            $token = "sAZJpFT7ntDM4+!gJ+h-";
+                
+            $curl = curl_init();
 
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.fonnte.com/send',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array(
+            'target' => $target,
+            'message' => 'Test API Send Message WA',
+
+            ),
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: $token"
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            // echo $response;
         json_encode($array);
 
-        //API Notifikasi WA
-        $token = "sAZJpFT7ntDM4+!gJ+h-";
-       
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://api.fonnte.com/send',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => array(
-        'target' => $target,
-        'message' => 'Test API Send Message WA',
-
-        ),
-        CURLOPT_HTTPHEADER => array(
-            "Authorization: $token"
-        ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        // echo $response;
+        
     }
 
     public function uploadPutusan()
