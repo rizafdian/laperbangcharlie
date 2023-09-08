49c9ab38a064a419ddc1250452aae32699beff7c
rizafdian <afdianriz@gmail.com>
2023-09-08 15:45:12 +0700

    update PP.4

--- assets/js/view_pp.js
+++ assets/js/view_pp.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
     $('#listperkara').on('click', '.item_view', function () {
         let data = list_perkara.row($(this).parents('tr')).data();
         let id_perkara = data['id_perkara'];
-        window.location.href = `${path}/pp/pp_laper/view_berkas_admin/${id_perkara}/`;
+        window.location.href = `${path}/pp/panitera_pengganti/view_berkas_admin/${id_perkara}/`;
 
     });
 
