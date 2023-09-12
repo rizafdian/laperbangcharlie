eebae06b115adc3b4fe8d607c1f254ab01267b42
rizafdian <afdianriz@gmail.com>
2023-09-12 09:53:50 +0700

    dashboard admin.7

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -7,36 +7,27 @@
         <div class="row">
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-satu text-white mb-4">
-                    <div class="card-body">Primary Card</div>
+                    <div class="card-body">PERKARA MASUK</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="#">View Details</a>
+                        <a class="small text-white stretched-link"><?php echo $putus_harian; ?> Perkara</a>
                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-dua text-white mb-4">
-                    <div class="card-body">Warning Card</div>
+                    <div class="card-body">PERKARA PUTUS</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="#">View Details</a>
+                        <a class="small text-white stretched-link"><?php echo $putus_harian; ?> Perkara</a>
                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-tiga text-white mb-4">
-                    <div class="card-body">Success Card</div>
+                    <div class="card-body">SISA PERKARA</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="#">View Details</a>
-                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-                <div class="card bg-empat text-white mb-4">
-                    <div class="card-body">Danger Card</div>
-                    <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="#">View Details</a>
+                        <a class="small text-white stretched-link"><?php echo $sisa_harian; ?> Perkara</a>
                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                     </div>
                 </div>
@@ -47,18 +38,18 @@
                 <div class="card mb-4">
                     <div class="card-header">
                         <i class="fas fa-chart-area me-1"></i>
-                        Area Chart Example
+                        Laporan Perkara Wilayah 1
                     </div>
-                    <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
+                    <div class="card-body"><canvas id="LaperZona1" width="100%" height="40"></canvas></div>
                 </div>
             </div>
             <div class="col-xl-6">
                 <div class="card mb-4">
                     <div class="card-header">
                         <i class="fas fa-chart-bar me-1"></i>
-                        Bar Chart Example
+                        Laporan Perkara Wilayah 2
                     </div>
-                    <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
+                    <div class="card-body"><canvas id="LaperZona2" width="100%" height="40"></canvas></div>
                 </div>
             </div>
         </div>
