d8f2a62aa5bdccf15342b3042356e2a9ea9257bd
rizafdian <afdianriz@gmail.com>
2023-09-14 10:24:13 +0700

    datatable PA.1

--- application/views/pa/pa_footer.php
+++ application/views/pa/pa_footer.php
@@ -22,11 +22,12 @@
 <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
 <script src="<?= base_url('bootstrap/') ?>js/datatables-simple-demo.js"></script>
 <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js" integrity="sha512-WNLxfP/8cVYL9sj8Jnp6et0BkubLP31jhTG9vhL/F5uEZmg5wEzKoXp1kJslzPQWwPT1eyMiSxlKCgzHLOTOTQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script>
     //datatable
     $(document).ready( function() {
-        $('#myTable').Datatable();
+        $('#dataTable1').Datatable();
     });
 </script>
 
