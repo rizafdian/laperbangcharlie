2a2c26276a897379031c7bceea466698cd4b288e
rizafdian <afdianriz@gmail.com>
2023-09-18 09:50:20 +0700

    update user PA.5

--- application/views/pa/laporan_bulanan.php
+++ application/views/pa/laporan_bulanan.php
@@ -77,7 +77,7 @@
               </th>
               <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status
               </th>
-              <th class="text-secondary opacity-7"></th>
+              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
             </tr>
           </thead>
           <tbody>
