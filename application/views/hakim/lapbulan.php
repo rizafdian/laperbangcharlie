783f276d18066d8de1d6d68f2bf1e76f260b5af3
rizafdian <afdianriz@gmail.com>
2023-09-21 09:09:12 +0700

    update user hakim.2

--- application/views/hakim/lapbulan.php
+++ application/views/hakim/lapbulan.php
@@ -9,9 +9,9 @@
         </div>
         </nav>
         <div class="tab-content" id="nav-tabContent">
-        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
-        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
-        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Home</div>
+        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">Profile</div>
+        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">Contact</div>
         </div>
 
         </div>
