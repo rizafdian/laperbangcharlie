1faae5db67dc9591e570e7f436fd17638be2a05e
rizafdian <afdianriz@gmail.com>
2023-09-22 14:55:46 +0700

    update laper hakim

--- application/controllers/hakim/Hakim_laper.php
+++ application/controllers/hakim/Hakim_laper.php
@@ -398,8 +398,8 @@ class Hakim_laper extends CI_Controller
         $this->session->set_flashdata('msg', 'Tidak ada File'); //kop pesannya
         $this->session->set_flashdata('properties', 'Anda tidak bisa mendowload file "PDF/XLS" karena Tidak ada filenya. !'); //isi pesannya.
 
-        $this->load->view('panitera_pengganti/header', $data);
+        $this->load->view('hakim/header', $data);
         $this->load->view('errors/view_message');
-        $this->load->view('panitera_pengganti/footer', $data);
+        $this->load->view('hakim/footer', $data);
     }
 }
