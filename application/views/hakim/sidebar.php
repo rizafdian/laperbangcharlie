240b8d348ed92a5b8a98a6fa79e08b072f9f4cb8
rizafdian <afdianriz@gmail.com>
2023-09-25 09:21:48 +0700

    laper hakim

--- application/views/hakim/sidebar.php
+++ application/views/hakim/sidebar.php
@@ -73,19 +73,19 @@
                         <div class="nav">
 
                             <div class="sb-sidenav-menu-heading">Laporan Perkara</div>
-                            <a class="nav-link <?= $side_judul == 'Laporan Bulanan' ? 'active' : '' ?>" href="<?= base_url('hakim/hakim_laper') ?>">
+                            <a class="nav-link <?= $judul == 'Laporan Bulanan' ? 'active' : '' ?>" href="<?= base_url('hakim/hakim_laper') ?>">
                                 <div class="sb-nav-link-icon"><i class="fas fa-fw fa-chart-pie"></i></div>
                                 Laporan Perkara
                             </a>
-                            <a class="nav-link <?= $side_judul == 'Rekap Laporan Bulanan' ? 'active' : '' ?>" href="<?= base_url('hakim/hakim_laper/rekap_laporan/') ?>">
+                            <a class="nav-link <?= $judul == 'Rekap Laporan Bulanan' ? 'active' : '' ?>" href="<?= base_url('hakim/hakim_laper/rekap_laporan/') ?>">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Rekap Laporan Perkara
                             </a>
-                            <a class="nav-link <?= $side_judul == 'Laporan Triwulan' ? 'active' : '' ?>" href="<?= base_url('hakim/hakim_laper/triwulan/') ?>">
+                            <a class="nav-link <?= $judul == 'Laporan Triwulan' ? 'active' : '' ?>" href="<?= base_url('hakim/hakim_laper/triwulan/') ?>">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Laporan Triwulan
                             </a>
-                            <a class="nav-link <?= $side_judul == 'Rekap Laporan Triwulan' ? 'active' : '' ?>" href="<?= base_url('hakim/hakim_laper/rekap_triwulan/') ?>">
+                            <a class="nav-link <?= $judul == 'Rekap Laporan Triwulan' ? 'active' : '' ?>" href="<?= base_url('hakim/hakim_laper/rekap_triwulan/') ?>">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Rekap Laporan Triwulan
                             </a>
