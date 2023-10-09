4fe10f993fa1b5521239f92dc06e028d56b00db5
rizafdian <afdianriz@gmail.com>
2023-10-09 14:34:22 +0700

    keterangan status bulan

--- application/controllers/pa/PA_laper.php
+++ application/controllers/pa/PA_laper.php
@@ -27,7 +27,9 @@ class PA_laper extends CI_Controller
         $data['laporan'] = $this->m_laper->get_data();
         $data['years'] = $this->m_laper->get_years();
         $data['current_year'] = date('Y');
-       
+        $data1['current_month'] = date('M-Y');
+        var_dump($data1);
+        die;
         $this->load->view('pa/pa_header');
         $this->load->view('pa/pa_sidebar');
         $this->load->view('pa/pa_topbar', $data);
