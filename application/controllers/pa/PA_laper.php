006a31d612be91d610320c6aed9ad59412898551
rizafdian <afdianriz@gmail.com>
2023-10-12 13:16:17 +0700

    penjagaan tambah perkara

--- application/controllers/pa/PA_laper.php
+++ application/controllers/pa/PA_laper.php
@@ -108,9 +108,10 @@ class PA_laper extends CI_Controller
 
     public function add_laporan_perkara()
     {
+        $id_user = $this->session->userdata('id');
         $data['judul'] = 'Laporan Perkara';
-        $query = $this->db->select('periode')->get('laporan_perkara');
-        $cek_periode['periodes'] = $query->result();
+        $query = $this->db->select('periode')->get_where('laporan_perkara', ['id' => $id_user])->result_array();
+        // $cek_periode['periodes'] = $query->result();
 
         //form validation rules
         $this->form_validation->set_rules('periode', 'Periode', 'required');
@@ -119,7 +120,7 @@ class PA_laper extends CI_Controller
 
             $periode = $this->input->post('periode', true);
             $periode_tgl = date('M Y', strtotime($periode));
-            var_dump($cek_periode, $periode_tgl);
+            var_dump($query, $periode_tgl, $id_user);
             die;
             $current_month = date('M Y');
             $next_month = date('M Y', strtotime('+1 month',strtotime($periode_tgl))) ;
